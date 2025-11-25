d0e83e8a6f500ffb68e6b86e90a2afc4f92da266
hawoonn <115150352+hawoonn@users.noreply.github.com>
2025-11-25 09:48:28 +0900

    new

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,9 @@ export default defineConfig(({ mode }) => {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        
       },
+      base: '/', 
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
